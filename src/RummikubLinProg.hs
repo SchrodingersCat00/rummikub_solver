e329b778c4753b07a841fa44127f4b1d88d6ed8e
Kobe <kobe.mertens@hotmail.com>
2021-01-18 18:10:12 +0100

    included glpk-hs

--- src/RummikubLinProg.hs
+++ src/RummikubLinProg.hs
@@ -26,8 +26,6 @@ objFun :: LinFunc String Int
 objFun = linCombination (zip (repeat 1) yVars)
 -- objFun = linCombination [(10, "x1"), (6, "x2"), (4, "x3")]
 
-n *& v = linCombination [(n,v)]
-
 lp :: UniqueSets -> Rack -> Table -> LP String Int
 lp sets r t = execLPM $ do
     setDirection Max
@@ -67,34 +65,9 @@ main = do
     -- print constr
     result <- glpSolveVars mipDefaults constr
     -- print sets
-    -- let constr = getConstraints sets rack table
-    -- print $ sum $ map (tileInSet sets 8) [0..1172]
-    -- let sol = simplex prob constr []
-    -- print sol
-    -- print $ parseLPResult sets sol
     print $ parseLPResult result
 
 parseLPResult (_, Just (v, d)) = M.foldrWithKey (\k v l -> if v > 0 then k:l else l) [] d
-
--- parseLPResult :: [[Tile RumNum Color]] -> Solution -> [(Int, [Tile RumNum Color])]
--- parseLPResult usets (Optimal (_, vars)) = map (\(i, _) -> (i, usets!!i)) $ filter (\(_, v) -> v == 1.0 || v == 2.0) (zip [0..1172] (drop 53 vars))
--- parseLPResult _ _ = error "Result not found"
-
--- tileInSet :: [[Tile RumNum Color]] -> Int -> Int -> Int
--- tileInSet ss tid sid
---     | tile `elem` set = 1
---     | otherwise       = 0
---   where
---     set = ss!!sid
---     tile = toEnum tid :: Tile RumNum Color
-
--- getConstraints :: UniqueSets -> Rack -> Table -> Constraints
--- getConstraints s r t = Sparse (c1 ++ c2 ++ c3 ++ c4)
---   where
---     c1 = [((-1.0,i+1):[(fromIntegral (countInSet (s!!j) (toEnum i)), 53+j+1) | j <- [0..1172]]) :==: fromIntegral (countInSet t (toEnum i)) | i <- [0..52]]
---     c2 = [[1#(i+1)] :<=: fromIntegral (countInSet r (toEnum i)) | i <- [0..52]]
---     c3 = [[1#(53+j+1)] :&: (0, 2) | j <- [0..1172]]
---     c4 = [[1#i] :&: (0, 2) | i <- [1..53]]
 
 countInSet :: Table -> Tile RumNum Color -> Int
 countInSet t tl = let c = countElem tl t in
@@ -102,14 +75,10 @@ countInSet t tl = let c = countElem tl t in
                             error "tile is more than twice on the table"
                         else c
 
--- main = do
---     let prob = Maximize [4, -3, 2]
---     let constr = Dense [ [2, 1, 0] :<=: 10
---                         , [0, 1, 5] :<=: 20
---                         ]
---     print $ simplex prob constr [ 2 :>=: 1, 3 :&: (2,7)]
-
 {-
+    source: https://www.semanticscholar.org/paper/Solving-Rummikub-Problems-by-Integer-Linear-Hertog-Hulshof/ab00c1bfe35e21a2edb7287234af03f74a3ee3ae
+
+    LP: specification
     indices
     -------
     i = type of tile defined by color and number
@@ -117,7 +86,7 @@ countInSet t tl = let c = countElem tl t in
 
     parameters
     ----------
-    s_ij = indicates wheter a tile is in a set (0 or 1)
+    s_ij = tile i appears 0, 1 or 2 times in set j
     t_i = tile i is 0, 1 or 2 times on the table
     r_i = tile i is 0, 1 or 2 times on your rack
 
@@ -132,16 +101,8 @@ countInSet t tl = let c = countElem tl t in
 
     constraints
     -----------
-    sum of all j's s_ij*x_j = t_i + y_i forall i
-    => [0,...,-1,...,0,s_i1,...,s_i1173] = t_i forall i
-
+    s_i1*x_1 + ... + s_i1173*x1173 = t_i + y_i forall i
     y_i <= r_i forall i
-    => [0,...,1,...,0,0,...,0] <= r_i forall i
-
     x_j el {0, 1, 2} forall j
-    => [0,...,0,0,...,1,...,0] el {0, 1, 2} forall j
-
     y_i el {0, 1, 2} forall i
-    => [0,...,1,...,0,0,...,0] el {0, 1, 2} forall i
-
 -}
